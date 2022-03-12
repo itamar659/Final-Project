c04b966bed48e623964bfa2878333ddc3e2b93e8
Daniel Caspit <dcaspit@gmail.com>
2022-03-12 09:18:57 +0200

    Maintenance

--- Project.UI/Pages/Host/HostHomePage.xaml.cs
+++ Project.UI/Pages/Host/HostHomePage.xaml.cs
@@ -4,15 +4,27 @@ namespace Project.UI;
 
 public partial class HostHomePage : ContentPage
 {
+	private HostViewModel vm;
 	public HostHomePage(HostViewModel hostViewModel)
 	{
 		InitializeComponent();
-
+		vm = hostViewModel;
 		BindingContext = hostViewModel;
 	}
 
 	private  void Button_Clicked(object sender, EventArgs e)
 	{
 		
+	}
+
+	private async void Left_Button_Clicked(object sender, EventArgs e)
+    {
+		bool answer = await DisplayAlert("Leaving already?", "Are you sure wanna leave ?", "Yes", "No");
+        if (answer)
+		{ 
+			Navigation.InsertPageBefore(new FindHostPage(vm.Host), Navigation.NavigationStack[0]);
+			await Navigation.PopToRootAsync();
+		}
+
 	}
 }
--- Project.UI/Pages/Host/HostPage.xaml.cs
+++ Project.UI/Pages/Host/HostPage.xaml.cs
@@ -12,7 +12,7 @@ public partial class HostPage : ContentPage
 
 		// Bad Initialize. Only for Practice.
 		// TODO: Add Host Object inorder to make it more expendabily
-		vm = new HostViewModel(host);
+		vm = new HostViewModel("Carla " + host);
 		vm.Song = "Kendrick Lamar: ADHD";
 		vm.ActiveUsers = "542";
 		vm.Genre = "Pop & Rock";
@@ -22,7 +22,19 @@ public partial class HostPage : ContentPage
 
 	private async void Button_Clicked(object sender, EventArgs e)
 	{
-		await Navigation.PushAsync(new HostHomePage(vm));
+		string result = await DisplayPromptAsync("2-Way Authentication", "Enter Host's personal pincode. for further information please contact the host.", keyboard: Keyboard.Numeric);
+
+		// TODO: make it dynamic password inside of the host object.
+		if(result == "1234")
+        {
+
+			Navigation.InsertPageBefore(new HostHomePage(vm), Navigation.NavigationStack[0]);
+			await Navigation.PopToRootAsync();
+        }
+        else
+        {
+			await DisplayAlert("Error", "Wrong Password. please try again.", "OK");
+        }
 	}
 
 }
