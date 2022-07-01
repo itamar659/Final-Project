c94ecd811e9affcdc8698c5890e4b5784bbf7935
Daniel Caspit <dcaspit@gmail.com>
2022-07-01 14:38:43 +0300

    Changes to main page

--- Client/Client/ViewModels/MainPageViewModel.cs
+++ Client/Client/ViewModels/MainPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Input;
+﻿using Client.Services;
+using System.Windows.Input;
 
 namespace Client;
 public class MainPageViewModel : BaseViewModel
@@ -6,9 +7,58 @@ public class MainPageViewModel : BaseViewModel
     public ICommand SignInCommand { get; set; }
     public ICommand GuestLoginCommand { get; set; }
 
-    public MainPageViewModel()
+
+    private readonly IServerApi _serverApi;
+
+    private string _errorMsgHolder;
+
+    public string Username { get; set; }
+    public string ErrorMsgHolder
     {
-        SignInCommand = new Command(async () => await Shell.Current.GoToAsync(nameof(LoginPage)));
-        GuestLoginCommand = new Command(async () => await Shell.Current.GoToAsync(nameof(GuestLoginPage)));
+        get => _errorMsgHolder;
+        set
+        {
+            _errorMsgHolder = value;
+            OnPropertyChanged(nameof(ErrorMsgHolder));
+        }
+    }
+
+    //public MainPageViewModel()
+    //{
+    //    //SignInCommand = new Command(async () => await Shell.Current.GoToAsync(nameof(LoginPage)));
+    //    //GuestLoginCommand = new Command(async () => await Shell.Current.GoToAsync(nameof(GuestLoginPage)));
+    //}    public ICommand AnonymousLoginCommand { get; set; }
+
+    public ICommand AnonymousLoginCommand { get; set; }
+
+
+    public MainPageViewModel(IServerApi serverAPI)
+    {
+        _serverApi = serverAPI;
+        AnonymousLoginCommand = new Command(connect);
+        SignInCommand = new Command(async () => await Shell.Current.GoToAsync($"{nameof(FindHostPage)}"));
+    }
+
+
+
+    private async void connect()
+    {
+        ErrorMsgHolder = string.Empty;
+        try
+        {
+            if (await _serverApi.AnonymousLoginAsync(Username))
+            {
+                await Shell.Current.GoToAsync(nameof(FindHostPage));
+            }
+            else
+            {
+                ErrorMsgHolder = "Error occurred";
+            }
+        }
+        catch (NullReferenceException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
     }
 }
