ed8f5edd2f96c96a67c28fe027fbe71218291b32
itamar659 <itamar.ayalon@gmail.com>
2022-07-02 18:02:07 +0300

    changed host login page

--- Host/Host/ViewModels/MainPageViewModel.cs
+++ Host/Host/ViewModels/MainPageViewModel.cs
@@ -8,11 +8,11 @@ using System.Windows.Input;
 namespace Host;
 public class MainPageViewModel : BaseViewModel
 {
-    public static readonly TimeSpan SERVER_UPDATE_DELAY = TimeSpan.FromMilliseconds(1000);
+    public static readonly double SERVER_UPDATE_DELAY = TimeSpan.FromSeconds(10).TotalMilliseconds;
 
     private IServerApi _serverAPI;
     private AudioPlayer _audioPlayer;
-    private bool _fetchUpdates;
+    private System.Timers.Timer _updateTimer;
 
     public BindableObject View { get; set; }
 
@@ -38,70 +38,23 @@ public class MainPageViewModel : BaseViewModel
     {
         _serverAPI = serverAPI;
         _audioPlayer = audioPlayer;
+        _audioPlayer.SongStateChanged += updateSongAsync;
+        _updateTimer = new System.Timers.Timer(SERVER_UPDATE_DELAY);
+        _updateTimer.Elapsed += fetchViewUpdateAsync;
 
         PollOptions = new ObservableCollection<PollOption>();
 
-        StartStopSessionCommand = new Command(async () =>
-        {
-            if (!IsSessionLive)
-            {
-                bool successfulOpen = await _serverAPI.OpenSessionAsync();
-
-                if (successfulOpen)
-                    StartFetchUpdates();
-            }
-            else
-            {
-                await _serverAPI.CloseSessionAsync();
-                StopFetchUpdates();
-
-                TotalUsers = 0;
-                ActiveUsers = 0;
-                OnPropertyChanged(nameof(TotalUsers));
-                OnPropertyChanged(nameof(ActiveUsers));
-            }
-
-            IsSessionLive = !IsSessionLive;
-
-            IsSessionLive = _serverAPI.GetSessionKey() != string.Empty;
-
-            OnPropertyChanged(nameof(IsSessionLive));
-        });
-
-        UpdateSongCommand = new Command(async () =>
-        {
-            //await _serverAPI.UpdateSongAsync(new SongUpdateRequest { SongName = SongName, Duration = Duration, Position = Position});
-        });
-
-        ChangeSessionPinCodeCommand = new Command(async () =>
-        {
-            await _serverAPI.ChangeSessionPinCodeAsync(SessionPinCode);
-        });
-
-        CreatePollCommand = new Command(async () =>
-        {
-            await _serverAPI.CreatePollAsync();
-        });
-
-        RemovePollCommand = new Command(async () =>
-        {
-            await _serverAPI.RemovePollAsync();
-        });
+        StartStopSessionCommand = new Command(startStopSession);
+        ChangeSessionPinCodeCommand = new Command(async () => await _serverAPI.ChangeSessionPinCodeAsync(SessionPinCode));
+        CreatePollCommand = new Command(async () => await _serverAPI.CreatePollAsync());
+        RemovePollCommand = new Command(async () => await _serverAPI.RemovePollAsync());
     }
 
-    public void StartFetchUpdates()
+    private async void fetchViewUpdateAsync(object sender, System.Timers.ElapsedEventArgs e)
     {
-        View.Dispatcher.DispatchDelayed(TimeSpan.Zero, FetchViewUpdateAsync);
-        _fetchUpdates = true;
-    }
+        if (IsSessionLive == false)
+            return;
 
-    public void StopFetchUpdates()
-    {
-        _fetchUpdates = false;
-    }
-
-    public async void FetchViewUpdateAsync()
-    {
         JukeboxSessionResponse sessionResponse = await _serverAPI.FetchSessionUpdateAsync();
         if (sessionResponse == null)
             return;
@@ -119,10 +72,31 @@ public class MainPageViewModel : BaseViewModel
 
         OnPropertyChanged(nameof(TotalUsers));
         OnPropertyChanged(nameof(ActiveUsers));
+    }
 
-        if (_fetchUpdates)
-            View.Dispatcher.DispatchDelayed(SERVER_UPDATE_DELAY, FetchViewUpdateAsync);
-
+    private async void updateSongAsync(object sender, EventArgs e)
+    {
         await _serverAPI.UpdateSongAsync(new SongUpdateRequest { SongName = _audioPlayer.SongName, Duration = _audioPlayer.Duration, Position = _audioPlayer.Position });
+    }
+
+    private async void startStopSession()
+    {
+        if (!IsSessionLive)
+        {
+            await _serverAPI.OpenSessionAsync();
+        }
+        else
+        {
+            await _serverAPI.CloseSessionAsync();
+
+            TotalUsers = 0;
+            ActiveUsers = 0;
+            OnPropertyChanged(nameof(TotalUsers));
+            OnPropertyChanged(nameof(ActiveUsers));
+        }
+
+        IsSessionLive = _serverAPI.GetSessionKey() != string.Empty;
+
+        OnPropertyChanged(nameof(IsSessionLive));
     }
 }
