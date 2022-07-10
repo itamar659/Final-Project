5dbec2c42624a90eeafbfc4f66b8aa73e2bab67a
itamar659 <itamar.ayalon@gmail.com>
2022-07-10 20:54:42 +0300

    poll response class

--- Client/Client/Models/Responses/JukeboxPollResponse.cs
+++ Client/Client/Models/Responses/JukeboxPollResponse.cs
@@ -7,4 +7,12 @@ using System.Threading.Tasks;
 namespace Client.Models.Responses;
 public class JukeboxPollResponse
 {
+    public class PollOption
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Votes { get; set; }
+    }
+
+    public List<PollOption> Options { get; set; }
 }
