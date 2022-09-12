4a0c807aed8d40531c5912d4a1ceeff4406ca903
itamar659 <itamar.ayalon@gmail.com>
2022-09-12 11:46:45 +0300

    status label converter

--- Host/Host/Converters/BooleanToStringConverter.cs
+++ Host/Host/Converters/BooleanToStringConverter.cs
@@ -4,13 +4,16 @@ namespace Host;
 
 public class BooleanToStringConverter : IValueConverter
 {
+    public string ValTrue { get; set; }
+    public string ValFalse { get; set; }
+
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        return (bool)value ? "||" : "I>";
+        return (bool)value ? ValTrue : ValFalse;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
-        return (string)value == "||";
+        return (string)value == ValTrue;
     }
 }
